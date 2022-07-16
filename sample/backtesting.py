21e06d997dcea636a9cf3f6212fc4464a4514c87
Herman Zhao <hermanzhaozzzz@gmail.com>
2022-07-17 02:25:40 +0800

    test

--- sample/backtesting.py
+++ sample/backtesting.py
@@ -1,13 +1,20 @@
 from datetime import datetime
 import backtrader as bt
+from pandas_datareader import data as pddata
+import pandas as pd
+
+
+# pip install pandas_datareader for yahoo data
+
 
 # Create a subclass of Strategy to define the indicators and logic
 
 class SmaCross(bt.Strategy):
+    """Doc."""
     # list of parameters which are configurable for the strategy
     params = dict(
         pfast=10,  # period for the fast moving average
-        pslow=30   # period for the slow moving average
+        pslow=30  # period for the slow moving average
     )
 
     def __init__(self):
@@ -24,12 +31,24 @@ class SmaCross(bt.Strategy):
             self.close()  # close long position
 
 
+# get data
+codes = ['aapl', 'ibm', 'msft', 'goog']  # 四个股票
+
+all_stock = {}
+
+for ticker in codes:
+    all_stock[ticker] = pddata.get_data_yahoo(ticker, start='1/1/2018', end='30/3/2018')  # 默认从2010年1月起始
+
+print(all_stock['msft'])
+
+
 cerebro = bt.Cerebro()  # create a "Cerebro" engine instance
 
 # Create a data feed
-data = bt.feeds.YahooFinanceData(dataname='MSFT',
-                                 fromdate=datetime(2011, 1, 1),
-                                 todate=datetime(2012, 12, 31))
+data = bt.feeds.PandasData(all_stock['msft'])
+# data = bt.feeds.YahooFinanceData(dataname='MSFT',
+#                                  fromdate=datetime(2011, 1, 1),
+#                                  todate=datetime(2012, 12, 31))
 
 cerebro.adddata(data)  # Add the data feed
 
